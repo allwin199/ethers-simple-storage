cb327129f1b215871d9e3f2579e02cd3e2a18a73
worksofallwin <princeallwin199@gmail.com>
2023-07-17 18:38:21 +0530

    Complete Deploy script and add prettier

--- deploy.js
+++ deploy.js
@@ -5,24 +5,28 @@ require("dotenv").config();
 // check also sampleDeploy for more info
 const main = async () => {
     const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-    const privateKey = process.env.PRIVATE_KEY;
-    const wallet = new ethers.Wallet(privateKey, provider);
+    const encryptedJson = fs.readFileSync("./.encryptedkey.json", "utf8");
+    let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+        encryptedJson,
+        process.env.PRIVATE_KEY_PASSWORD,
+    );
+    wallet = await wallet.connect(provider);
 
     // get contract files
     const abi = fs.readFileSync(
         "./SimpleStorage_sol_SimpleStorage.abi",
-        "utf8"
+        "utf8",
     );
     const binary = fs.readFileSync(
         "./SimpleStorage_sol_SimpleStorage.bin",
-        "utf8"
+        "utf8",
     );
 
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
     console.log("Deploying, Please Wait...........");
     const contract = await contractFactory.deploy();
 
-    const transactionReceipt = await contract.deployTransaction.wait(1);
+    await contract.deployTransaction.wait(1);
 
     console.log("Deployment Successful");
     // console.log("Contract", transactionReceipt);
