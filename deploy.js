3424e5ee6b9b3b1e0103a2a20b76393fa171530c
worksofallwin <princeallwin199@gmail.com>
2023-07-17 13:21:34 +0530

    Add ethers js

--- deploy.js
+++ deploy.js
@@ -1,4 +1,17 @@
-const main = async () => {};
+const { ethers } = require("ethers");
+// const fs = require("fs-extra");
+
+const main = async () => {
+    // http://127.0.0.1:7545
+    const provider = new ethers.providers.JsonRpcProvider(
+        "http://127.0.0.1:7545"
+    );
+    const blockNumber = await provider.getBlockNumber();
+    const privateKey =
+        "0x5568080dc28b6905e036cde74302cb60931d7c3dbdf6751c7c5f15973f5e3e0a";
+    const wallet = new ethers.Wallet(privateKey, provider);
+    // const signer = provider.getSigner();
+};
 
 main()
     .then(() => process.exit(0))
@@ -6,3 +19,5 @@ main()
         console.error(error);
         process.exit(1);
     });
+
+// Ethers js helps us to interact with the blockchain.
